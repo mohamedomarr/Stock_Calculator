cdcaac8f18b8cc4e372f270cbe563b576ff58201
mohamed omar <mohamedsomarr@gmail.com>
2021-01-26 17:57:55 +0200

    add percentage and profit margin

--- calc.py
+++ calc.py
@@ -13,12 +13,13 @@ print(f"Day {day}: {number_with_commas1} USD & {number_with_commasEG} EGP")
 day +=1
 
 for x in range(Number_of_days):
-    after_1_day_per = Total_cash * 0.04
+    after_1_day_per = Total_cash * 0.025
     Total_cash += after_1_day_per
     intnumber = int(Total_cash)
     number_with_commas = "{:,}".format(intnumber)
     number_with_commasEG = "{:,}".format(int(int(Total_cash) * EGP))
-    print(f"Day {day}: {number_with_commas} USD & {number_with_commasEG} EGP")
+    #print(f"Day {day}: {number_with_commas} USD & {number_with_commasEG} EGP")
     day +=1
     
-print(f"Total after {Number_of_days} days is {number_with_commas} USD & {number_with_commasEG} EGP")
+print(f"Total After {Number_of_days} days is {number_with_commas} USD & {number_with_commasEG} EGP")
+print(f"Profit Margin in {Number_of_days} days is {int(Total_cash / main_cash) * 100}% and {int(Total_cash / main_cash)} Times more")
