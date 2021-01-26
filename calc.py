14ff7465e472c2db264dfd300094bec8465f3601
mohamed omar <mohamedsomarr@gmail.com>
2021-01-26 19:03:50 +0200

    Update calc.py

--- calc.py
+++ calc.py
@@ -2,6 +2,7 @@
 main_cash = 500
 EGP= 15.73
 Number_of_days = 252
+Profit_Percentage_per_day = 2.5
 
 # Don't miss with Above
 Total_cash = main_cash
@@ -13,7 +14,7 @@ print(f"Day {day}: {number_with_commas1} USD & {number_with_commasEG} EGP")
 day +=1
 
 for x in range(Number_of_days):
-    after_1_day_per = Total_cash * 0.03
+    after_1_day_per = Total_cash * (Profit_Percentage_per_day / 100)
     Total_cash += after_1_day_per
     intnumber = int(Total_cash)
     number_with_commas = "{:,}".format(intnumber)
