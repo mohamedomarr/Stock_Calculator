edf9c13bb9bd024c31ae1a60178110f34aaae892
mohamed omar <mohamedsomarr@gmail.com>
2021-01-27 11:56:34 +0200

    Update calc.py

--- calc.py
+++ calc.py
@@ -2,7 +2,7 @@
 main_cash = 500
 EGP= 15.73
 Number_of_days = 252
-Profit_Percentage_per_day = 2.5
+Profit_Percentage_per_day = 3
 
 # Don't miss with Above
 Total_cash = main_cash
@@ -22,8 +22,7 @@ for x in range(Number_of_days):
     #print(f"Day {day}: {number_with_commas} USD & {number_with_commasEG} EGP")
     day +=1
 
-per_with_commas1 = "{:,}".format(int(Total_cash / main_cash))
-per_with_commas2 = "{:,}".format(int(Total_cash / main_cash) * 100)
+per_with_commas_per = "{:,}".format(int(((Total_cash - main_cash) / main_cash) * 100))
 
 print(f"Total After {Number_of_days} days is {number_with_commas} USD & {number_with_commasEG} EGP")
-print(f"Profit Margin in {Number_of_days} days is {per_with_commas2}% and {per_with_commas1} Times")
+print(f"Profit Margin in {Number_of_days} days is {per_with_commas_per}%")
